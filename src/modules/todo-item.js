ceb78673fe942f290263e0b9867426813a764e88
Kyle Jester <95105911+kjester15@users.noreply.github.com>
2024-08-14 16:23:20 -0700

    Create prelim Task class in task-item module

---
+++ src/modules/todo-item.js
@@ -0,0 +1,8 @@
+export default class Task {
+  constructor(title, date, priority, note) {
+    this.title = title;
+    this.date = date;
+    this.priority = priority;
+    this.note = note;
+  }
+}
