8dc3459f4bc4e165105fde0e9a301951fb0aa60a
Kyle Jester <95105911+kjester15@users.noreply.github.com>
2024-08-26 15:24:56 -0700

    Add style loader to webpack

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import Task from "./modules/task";
 import List from "./modules/list";
+import './style.css';
 
 const newList = new List("project 1", "the best project to ever exist")
 console.log(newList);
