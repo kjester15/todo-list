df3582e79fc029bf832e78fada8f17ce1e7bd279
Kyle Jester <95105911+kjester15@users.noreply.github.com>
2024-08-27 15:57:11 -0700

    Create form and listener to add list to user and display after submitting

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import Task from "./modules/task";
-import List from "./modules/list";
+import { List, addList } from "./modules/list";
 import User from "./modules/user";
 import { clearLists, displayLists, openListDialog, closeListDialog } from "./modules/display";
 import './style.css';
@@ -11,8 +11,40 @@ const user = new User;
 let newList = document.getElementById("new-list");
 newList.addEventListener("click", function() {
   openListDialog();
-  const list = new List('test title', 'test description');
-  user.lists.push(list);
+});
+
+// Handle new list form
+const listForm = document.getElementById("list-form");
+listForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+  new FormData(listForm);
+});
+
+listForm.addEventListener("formdata", (event) => {
+  const list = {}
+  const data = event.formData;
+  data.forEach((value, key) => (list[`${key}`] = value))
+  addList(user, list)
+  listForm.reset();
+  closeListDialog();
   clearLists();
   displayLists(user.lists);
 });
+
+// Handle new task form
+// const taskForm = document.getElementById("task-form");
+// taskForm.addEventListener("submit", (event) => {
+//   event.preventDefault();
+//   new FormData(taskForm);
+// });
+
+// taskForm.addEventListener("formdata", (event) => {
+//   const task = {}
+//   const data = event.formData;
+//   data.forEach((value, key) => (list[`${key}`] = value))
+//   addTask(list, task)
+//   taskForm.reset();
+//   closeTaskDialog();
+//   clearTasks();
+//   displayTasks(list.tasks);
+// });
--- src/modules/list.js
+++ src/modules/list.js
@@ -1,7 +1,12 @@
-export default class List {
+export class List {
   constructor(title, description) {
     this.title = title;
     this.description = description;
     this.tasks = [];
   }
+};
+
+export const addList = (user, list) => {
+  const newList = new List(list.title, list.description);
+  user.lists.push(newList);
 };
